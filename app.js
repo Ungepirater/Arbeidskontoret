d5c8f5aa178d469ddcb8c6a45b04855553490ca0
Fredrik Mundt <famthegeek95@gmail.com>
2014-05-22 13:45:52 +0200

    Added bootstrap from bower and began work on frontpage template

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,8 +18,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', function(req, res) {
+  res.render('index', { title: 'Arbeidskontoret' });
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
